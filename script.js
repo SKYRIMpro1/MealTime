bff6a13a33aa3f0db936cb36aaea01396f8521c2
JoNoC0 <jnguyeningco@gmail.com>
2021-10-29 20:31:09 -0500

    food search

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-console.log('loaded')
-fetch('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=Cheddar%20Cheese')
-.then(function(response){
-    return response.json()
-}).then(function(data){
-    console.log(data)
-})
+var foodInput = document.getElementById('food-search');
+var foodRecipeEl = document.getElementById('food-recipe');
 
+var apiKey = "o7645HZzM0ujmdeIjvnj4W328P77zYuFTWFGoXhL";
+
+console.log('loaded')
+
+function getFoodInfo() {
+    var foodNameEl = document.getElementById("food-search")
+    var apiFoodUsda = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=' + apiKey + '&query=' + foodNameEl.value
+
+fetch(apiFoodUsda)
+    .then(function(response) {
+        return response.json();
+})
+    .then(function(data) {
+    foodRecipeEl.innerHTML = "Recipe: " + (data.foods[0].value);
+    console.log(data)
+});
+}
+
+var submitBtn = document.getElementById("search-btn");
+submitBtn.addEventListener("click", getFoodInfo);
