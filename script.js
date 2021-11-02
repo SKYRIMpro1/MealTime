22970c2fb451a0447a58b1b1b93d1a375c8e3fa5
JoNoC0 <jnguyeningco@gmail.com>
2021-11-02 17:15:05 -0500

    recipe description

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 var foodInput = document.getElementById('food-search');
-var foodRecipeEl = document.getElementById('food-recipe');
+var recipeNameEl = document.getElementById('recipe-name');
+var ingredientsEl = document.getElementById('ingredients');
+var recipeDescEl = document.getElementById('recipe-desc')
+var recipePicEl = document.getElementsByTagName('recipe-pic');
 
 var apiKey = "o7645HZzM0ujmdeIjvnj4W328P77zYuFTWFGoXhL";
 
-console.log('loaded')
 
 function getFoodInfo() {
     var foodNameEl = document.getElementById("food-search")
-    var apiFoodUsda = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=' + apiKey + '&query=' + foodNameEl.value
-
-fetch(apiFoodUsda)
+    // var apiFoodUsda = 'https://api.nal.usda.gov/fdc/v1/foods/search?api_key=' + apiKey + '&query=' + foodNameEl.value
+    var mealDB = 'https://www.themealdb.com/api/json/v1/1/search.php?s=' + foodNameEl.value
+fetch(mealDB)
     .then(function(response) {
         return response.json();
 })
     .then(function(data) {
-    foodRecipeEl.innerHTML = "Recipe: " + (data.foods[0].value);
-    console.log(data)
+    recipeNameEl.innerHTML = "Recipe Name: " + (data.meals[0].strMeal);
+    recipePicEl.innerHTML = "Pictures: " + (data.meals[0].strYouTube);
+    recipeDescEl.innerHTML = "Recipe Instructions: " + (data.meals[0].strInstructions);
+    console.log(data.meals[0].strMeal) 
+    ingredientsEl.innerHTML = "Ingredients: " + (data.meals[0].strIngredient1);
+    
+    var ingredientsArr = []
+    
+    for (let i = 0; i < ingredientsArr.length; i++) {
+    console.log(ingredientsArr);
+    }
 });
+
 }
 
 var submitBtn = document.getElementById("search-btn");
